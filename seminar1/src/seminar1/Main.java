4832ec1f933dd5f9fb1c6557f57375bd4c970aee
floretinapopa <floretinapopa67@gmail.com>
2021-02-27 11:22:06 +0200

    Pug class added

--- seminar1/src/seminar1/Main.java
+++ seminar1/src/seminar1/Main.java
@@ -2,6 +2,7 @@ package seminar1;
 
 import seminar1_clase.Animal;
 import seminar1_clase.Lion;
+import seminar1_clase.Pug;
 import seminar1_clase.Zoo;
 
 public class Main {
@@ -13,6 +14,10 @@ public class Main {
 		Lion lion2=new Lion("Nala");
 		zoo.addAnimal(lion1);
 		zoo.addAnimal(lion2);
+		Pug pug1=new Pug("Emma");
+		Pug pug2=new Pug("Stark",3);
+		zoo.addAnimal(pug1);
+		zoo.addAnimal(pug2);
 		zoo.feedAnimals();
 	}
 
---
+++ seminar1/src/seminar1_clase/Pug.java
@@ -0,0 +1,15 @@
+package seminar1_clase;
+
+public class Pug extends Animal{
+
+	private int age;
+	public Pug(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	public Pug(String name, int age) {
+		super(name);
+		this.age = age;
+	}
+
+}
