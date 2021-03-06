38f2c685909aba4a8da803798f0e06ebdd8dd281
floretinapopa <floretinapopa67@gmail.com>
2021-03-06 10:43:32 +0200

    Created reader interface and modified classes

--- seminar2/src/ro/ase/cts/program/Program.java
+++ seminar2/src/ro/ase/cts/program/Program.java
@@ -4,16 +4,22 @@ import java.io.FileNotFoundException;
 import java.util.List;
 
 import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
 import ro.ase.cts.reader.AngajatReader;
+import ro.ase.cts.reader.AplicantReader;
+import ro.ase.cts.reader.StudentReader;
 
 public class Program {
+	public static List<Aplicant> citireAplicanti(String fisier, AplicantReader reader) throws FileNotFoundException {
+		return reader.readAplicanti(fisier);
+	}
 
 	public static void main(String[] args) {
-		List<Angajat> listaAngajati;
+		List<Aplicant> lista;
 		try {
-			listaAngajati = AngajatReader.readAngajati("angajati.txt");
-			for(Angajat angajat:listaAngajati)
-				System.out.println(angajat.toString());
+			lista = citireAplicanti("elevi.txt",new StudentReader());
+			for(Aplicant a:lista)
+				System.out.println(a.toString());
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
