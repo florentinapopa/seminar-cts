29a537ed1269fa25a4d45a22e5ad2a7961875ee1
floretinapopa <floretinapopa67@gmail.com>
2021-05-22 15:59:09 +0300

    JUnit

--- seminar12/src/ro/ase/cts/tests/StudentTests.java
+++ seminar12/src/ro/ase/cts/tests/StudentTests.java
@@ -37,5 +37,63 @@ public class StudentTests {
 		student.adaugaNota(nota);
 		assertEquals(nota, student.getNota(0));
 	}
+	
+	@Test
+	public void testDimensiuneLista() {
+		Student student=new Student();
+		student.adaugaNota(10);
+		student.adaugaNota(10);
+		assertEquals(2, student.getNote().size());
+	}
+	
+	@Test 
+	public void testCalculMedie() {
+		Student student=new Student();
+		student.adaugaNota(10);
+		student.adaugaNota(7);
+		//adaugam param delta cand avem float
+		assertEquals(8.5f, student.calculeazaMedie(),0.01);
+	}
+	
+	@Test
+	public void testStudentCuRestante() {
+		Student student=new Student();
+		student.adaugaNota(4);
+		assertTrue(student.areRestante());
+	}
+	
+	@Test
+	public void testStudentFaraRestante() {
+		Student student=new Student();
+		student.adaugaNota(8);
+		assertFalse(student.areRestante());
+	}
+	//specific junit 3
+	@Test
+	public void testIndexInvalid() {
+		Student student=new Student();
+		student.adaugaNota(6);
+		//1
+		try {
+			//2
+			int nota=student.getNota(-1);
+			//3->nu se ajunge
+			fail("Metoda nu arunca exceptie");
+		} catch (IndexOutOfBoundsException e) {
+			// 4
+		} catch(Exception ex) {
+			//5->nu se ajunge
+			fail("Metoda arunca alta exceptie");
+		}
+		//6
+	}
+	//specific junit 4
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testIndexInvalidJUnit4() {
+		Student student=new Student();
+		student.adaugaNota(6);
+		int nota=student.getNota(-1);
+			
+	}
 
 }
