dd0aaec5fc7f3b3afa419cadfdc50b3ef2a29f96
floretinapopa <floretinapopa67@gmail.com>
2021-06-05 10:30:30 +0300

    Added tests + mock classes

---
+++ seminar13/src/ro/ase/cts/teste/TestPromovabilitateGrupaWithFixture.java
@@ -0,0 +1,29 @@
+package ro.ase.cts.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
+
+public class TestPromovabilitateGrupaWithFixture {
+private Grupa grupa;
+	
+	@Before
+	public void setUp() {
+		grupa=new Grupa(1070);
+		for(int i=0;i<35;i++) {
+			Student student=new Student("Gigel");
+			student.adaugaNota(8);
+			student.adaugaNota(8);
+			grupa.adaugaStudent(student);
+		}
+	}
+	
+	@Test (timeout = 500)
+	public void testPerformancePentruTimp() {
+		grupa.getPromovabilitate();
+	}
+}
---
+++ seminar13/src/ro/ase/cts/teste/TestePromovabilitateGrupa.java
@@ -0,0 +1,71 @@
+package ro.ase.cts.teste;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ro.ase.cts.clase.Grupa;
+import ro.ase.cts.clase.Student;
+import ro.ase.cts.dubluri.StudentDummy;
+
+public class TestePromovabilitateGrupa {
+	
+	
+
+	@Test
+	public void testRight() {
+		Grupa grupa=new Grupa(1085);
+		for(int i=0;i<3;i++) {
+			Student student=new Student("Gigel");
+			student.adaugaNota(4);
+			student.adaugaNota(5);
+			grupa.adaugaStudent(student);
+		}
+		for(int i=0;i<3;i++) {
+			Student student=new Student("Gigel");
+			student.adaugaNota(9);
+			student.adaugaNota(5);
+			grupa.adaugaStudent(student);
+		}
+		assertEquals(0.5, grupa.getPromovabilitate(),0.01);
+	}
+	
+	
+	@Test
+	public void testBoundaryInferior() {
+		Grupa grupa=new Grupa(1085);
+		for(int i=0;i<6;i++) {
+			Student student=new Student("Gigel");
+			student.adaugaNota(2);
+			student.adaugaNota(8);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(0, grupa.getPromovabilitate(),0.01);
+	}
+	
+	@Test
+	public void testBoundarySuperior() {
+		Grupa grupa=new Grupa(1085);
+		for(int i=0;i<6;i++) {
+			Student student=new Student("Gigel");
+			student.adaugaNota(8);
+			student.adaugaNota(8);
+			grupa.adaugaStudent(student);
+		}
+		
+		assertEquals(1, grupa.getPromovabilitate(),0.01);
+	}
+	
+	@Test (expected=IndexOutOfBoundsException.class)
+	public void testError() {
+		Grupa grupa=new Grupa(1011);
+		grupa.getPromovabilitate();
+	}
+	
+	
+	
+	
+	
+}
